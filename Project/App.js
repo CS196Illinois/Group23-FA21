cfcd255187737a49ec73df49ef47a3550c503003
jessmho <90438568+jessmho@users.noreply.github.com>
2021-10-10 15:15:57 -0500

    react app
    basic file to build on

---
+++ Project/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Zing </h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          CS196 App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
