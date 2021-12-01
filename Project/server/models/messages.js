901cbfde0babace9d8144575f116839fa6555b29
Milnil <90474265+Milnil@users.noreply.github.com>
2021-12-01 16:16:44 -0600

    Create messages.js

---
+++ Project/server/models/messages.js
@@ -0,0 +1,23 @@
+const db = require('../database');
+
+class Messages {
+    static retrieveALL (callback) {
+        //Change name of database here if necessary
+        db.query('SELECT messages_text from messages4', (err, res) => {
+            if (err.error)
+                return callback(err);
+            callback(res);
+        });
+    }
+
+    static insert (message, callback) {
+        //Change name of database here if necessary
+        db.query('INSERT INTO messages4 (messages_text) VALUES ($1)', [message], (err, res) => {
+            if (err.error)
+                return callback(err);
+            callback(res);
+        });
+    }
+}
+
+module.exports = Messages;
