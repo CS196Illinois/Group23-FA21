8262b653b0f4be5ef931df675778986d1d2a2eb8
Milnil <90474265+Milnil@users.noreply.github.com>
2021-12-01 20:22:45 -0600

    Update messages.js

--- Project/server/models/messages.js
+++ Project/server/models/messages.js
@@ -12,6 +12,12 @@ class Messages {
 
     static insert (message, callback) {
         //Change name of database here if necessary
+        const words = ['shit', 'fuck', 'bitch', 'hell', 'bastard', 'dick'];
+        
+        if (words.includes(message.toString())) {
+            return;
+        }
+         
         db.query('INSERT INTO messages4 (messages_text) VALUES ($1)', [message], (err, res) => {
             if (err.error)
                 return callback(err);
